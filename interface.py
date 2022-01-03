e67bc060dff1b3a139b16ddad714214a0ec16097
Farhang Bakhshi <farhang.bakhshi@gmail.com>
2022-01-03 20:25:01 +0330

    while True moved to interface

--- interface.py
+++ interface.py
@@ -1,3 +1,5 @@
+from contact import Contact
+
 class Interface:
     main_menu = ["1. view contact list", "2. search in contacts", "3. add a contact", "4. quit"]
     
@@ -9,3 +11,15 @@ class Interface:
             print(self.main_menu[x])
         response = int(input("? "))
         return response
+
+    def start(self):
+        while True:
+            user_choice = self.show_menu()
+            if user_choice == 4:
+                quit()
+            elif user_choice == 3:
+                new_contact_name = input("what is the name? ")
+                new_contact_number = input("what is the number? ")
+                new_contact_address = input("what is the address? ")
+                new_contact = Contact(new_contact_name, new_contact_number, new_contact_address)
+                print(new_contact)
--- runner.py
+++ runner.py
@@ -1,5 +1,3 @@
-import json
-from contact import Contact
 from interface import Interface
 from contact_book_core import Core
 from file_handler import FileHandler
@@ -8,15 +6,4 @@ file_handler = FileHandler()
 core = Core(file_handler)
 interface = Interface(core)
 
-while True:
-    user_choice = interface.show_menu()
-    if user_choice == 4:
-        quit()
-    elif user_choice == 3:
-        new_contact_name = input("what is the name? ")
-        new_contact_number = input("what is the number? ")
-        new_contact_address = input("what is the address? ")
-        new_contact = Contact(new_contact_name, new_contact_number, new_contact_address)
-        print(new_contact)
-
-
+interface.start()
