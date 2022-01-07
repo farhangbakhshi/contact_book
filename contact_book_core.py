b8ad012052101c111c6b31a7bb5a39966edb40b4
Farhang Bakhshi <farhang.bakhshi@gmail.com>
2022-01-07 04:05:47 +0330

    feat: edit and delete.

--- contact_book_core.py
+++ contact_book_core.py
@@ -5,7 +5,7 @@ from contact import Contact
 
 def key_function(contact):
     '''
-    this function gets a contact and returns its name to be used in a sort function as the key to sort. SORT IS DONE IN ADD_CONTACT().
+    this function gets a contact and returns its name to be used in a sort function as the key to sort. SORT IS DONE IN ADD_CONTACT() AND EDIT_CONTACT().
     '''
     return contact.name
 
@@ -33,13 +33,8 @@ class Core:
         #appending it to the contact_list and sorting that list to make it ready for writing to the file and possible future view_contact_list actions
         self.contact_list.append(new_contact)
         self.contact_list.sort(key = key_function)
-        #preparing the string for file by turning each contact into a dictionary and adding them all to a list
-        list_for_json = []
-        for x in range(len(self.contact_list)):
-            contact_in_dict = {"name":self.contact_list[x].name, "number":self.contact_list[x].number, "address":self.contact_list[x].address, "id":self.contact_list[x].id}
-            list_for_json.append(contact_in_dict)
-        #now we use dumps() to prepare the list for the json file and then call write_file() to save the new list to the file.
-        self.file_handler.write_file(json.dumps(list_for_json))
+        #now we can write that to file
+        self.write_current_contact_list_to_file()
 
     def get_contacts_id_list(self):
         '''
@@ -75,3 +70,52 @@ class Core:
         #then we return the other details of that contact's profile to the interface
         details = [intended_contact.name, intended_contact.number, intended_contact.address]
         return details
+
+    def edit_contact(self, id, detail_to_edit, new_detail):
+        '''
+        this is called from edit_contact in interface. to see what each detail_to_edit value means, you can see that method. it's easier to understand there.
+        '''
+        #first we find that contact in contact_list and write its index in a variable
+        contact_to_edit_index = None
+        for x in range(len(self.contact_list)):
+            if self.contact_list[x].id == id:
+                contact_to_edit_index = x
+                break
+        
+        #now we have to put that new detail in place
+        if detail_to_edit == 1:
+            self.contact_list[contact_to_edit_index].name = new_detail
+            self.contact_list.sort(key = key_function)
+        elif detail_to_edit == 2:
+            self.contact_list[contact_to_edit_index].number = new_detail
+        elif detail_to_edit == 3:
+            self.contact_list[contact_to_edit_index].address = new_detail
+        #now we have to write that contact list to database
+        self.write_current_contact_list_to_file()
+
+
+    def delete_contact(self, id):
+        '''
+        this is called from interface. useful for toxic people around you.
+        '''
+        #first we find that contact in contact_list and write its index in a variable
+        contact_to_delete_index = None
+        for x in range(len(self.contact_list)):
+            if self.contact_list[x].id == id:
+                contact_to_delete_index = x
+                break
+        #now we delete it
+        del self.contact_list[contact_to_delete_index]
+        self.write_current_contact_list_to_file()
+        
+    def write_current_contact_list_to_file(self):
+        '''
+        called from add, edit , and delete, this is a useful method!
+        '''
+        #preparing the string for file by turning each contact into a dictionary and adding them all to a list
+        list_for_json = []
+        for x in range(len(self.contact_list)):
+            contact_in_dict = {"name":self.contact_list[x].name, "number":self.contact_list[x].number, "address":self.contact_list[x].address, "id":self.contact_list[x].id}
+            list_for_json.append(contact_in_dict)
+        #now we use dumps() to prepare the list for the json file and then call write_file() to save the new list to the file.
+        self.file_handler.write_file(json.dumps(list_for_json))
--- interface.py
+++ interface.py
@@ -1,60 +1,111 @@
-from contact import Contact
+from contact_book_core import Core
 
 class Interface:
-    main_menu = ["1. view contact list", "2. search in contacts", "3. add a contact", "4. quit"]
-    contact_view_menu = ["1. edit", "2. delete"]
+    menus = {
+        "main_menu": ["1. view contact list", "2. search in contacts", "3. add a contact", "4. quit"],
+        "contact_list_view_menu": ["choose a contact by number to show details or use 0 to go back to main menu."],
+        "contact_details_view_menu": ["1. edit", "2. delete", "0. main menu"],
+        "edit_menu": ["1. name", "2. number", "3. address"]
+    }
 
-    def __init__(self, core):
+    def __init__(self, core:Core):
         self.core = core
 
-    def show_menu(self):
-        for x in range(len(self.main_menu)):
-            print(self.main_menu[x])
-        response = int(input("? "))
-        return response
+    def show_menu(self, menu_name):
+        '''
+        this method is made only to clean up the clutter in start()
+        '''
+        #algorithm is pretty obvious.
+       
+        for x in self.menus[menu_name]:
+            print(x)
+        
+        user_choice = int(input("? "))
+        return user_choice
 
     def start(self):
+        '''
+        this is the method that uses core to run all user requests.
+        '''
+        #this while True loop is used to constantly get user requests while staying in the program until the user chooses to quit the program
         while True:
-            user_choice = self.show_menu()
-            if user_choice == 4:
-                quit()
+            user_choice = self.show_menu("main_menu")
+            
+            if user_choice == 1:
+                #in this case we first have to get the list of all contacts' IDs
+                id_list = self.core.get_contacts_id_list()
+                self.contact_list_choices(id_list)
+            
+            elif user_choice == 2:
+                #first we need a search phrase
+                s_phrase = input("enter the phrase you want to search for. \n")
+                results_id_list = self.core.search(s_phrase)
+                self.contact_list_choices(results_id_list)
             
             elif user_choice == 3:
                 self.add_contact()
             
-            elif user_choice == 1:
-                contact_list = self.core.get_contact_names()
-                
-                for x in range(len(contact_list)):
-                    print(str(x + 1) + ". " + contact_list[x])
-                
-                self.view_contact_details("")
-            
-            elif user_choice == 2:
-                s_phrase = input("search for: ")
-                results_list = self.core.search(s_phrase)
-                
-                for x in range(len(results_list)):
-                    print(str(x + 1) + ". " + results_list[x])
-                
-                self.view_contact_details(s_phrase)
+            elif user_choice == 4:
+                quit()
                 
                 
 
     def add_contact(self):
+        '''
+        this method, like many others, is made to avoid the clutter in start() method
+        '''
+        #algorithm is pretty obvious.
         new_contact_name = input("what is the name? ")
         new_contact_number = input("what is the number? ")
         new_contact_address = input("what is the address? ")
         self.core.add_contact(new_contact_name, new_contact_number, new_contact_address)
-
-    def view_contact_details(self, s_phrase):
-        print("select a contact to show details (enter 0 to go back to main menu)")
-        contact_choice = int(input("? "))
-                
-        if contact_choice == 0:
+    
+    def contact_list_choices(self, id_list):
+        '''
+        this method, like many others, is made to avoid the clutter in start() method
+        '''
+        #first we have to make a list of lists in which each item is a list of the contact's other details. remember that these two lists have similar indexes for each contact. one storing the ID and the other a list of that ID's other attributes.
+        details_list = []
+        for x in id_list:
+            details_list.append(self.core.get_contact_details(x))
+        #then we print the list of names and ask the user to either choose one or go back to the main menu
+        for x in range(len(details_list)):
+            print(str(x + 1) + ". " + details_list[x][0])
+        user_contact_choice = self.show_menu("contact_list_view_menu")
+        #now we have to deal with that user choice
+        if user_contact_choice == 0:
             return
         else:
-                contact_details = self.core.get_contact_details(contact_choice, s_phrase)
-                print("name: " + contact_details[0])
-                print("number: " + contact_details[1])
-                print("address: " + contact_details[2])
+            print("name: " + details_list[user_contact_choice - 1][0])
+            print("number: " + details_list[user_contact_choice - 1][1])
+            print("address: " + details_list[user_contact_choice - 1][2])
+        #now we show the user another menu to choose what to do with this contact
+        user_choice_in_detail_view = self.show_menu("contact_details_view_menu")
+        #and again, dealing with that choice...
+        if user_choice_in_detail_view == 0:
+            return
+        elif user_choice_in_detail_view == 1:
+            #this one will need some info that should be gotten from the user before we call an edit for that contact, so this one goes to a method inside this same class first
+            self.edit_contact(id_list[user_contact_choice - 1])
+            print("user successfully edited.")
+        elif user_choice_in_detail_view == 2:
+            #but this one can directly go to core. we don't need any more info.
+            self.core.delete_contact(id_list[user_contact_choice - 1])
+            print("user deleted.")
+        #... and, it seems like we are finally done with the option.
+
+    def edit_contact(self, id):
+        '''
+        this method is made to view the menu and ask the question about the edit in contact, and then return to the contact_list_choices method (where it's called)
+        '''
+        #first we use our dear show_menu to show the menu
+        detail_to_edit = self.show_menu("edit_menu")
+        #then we ask the right question
+        if detail_to_edit == 1:
+            new_detail = input("what is the new name?\n")
+        elif detail_to_edit == 2:
+            new_detail = input("what is the new number?\n")
+        elif detail_to_edit == 3:
+            new_detail = input("what is the new address?\n")
+        #then we give it to the core (where this detail_to_edit number means the same thing as above)
+        self.core.edit_contact(id, detail_to_edit, new_detail)
